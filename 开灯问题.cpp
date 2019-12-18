2e97297c756f6216d531631ec150f736b08086ee
liwanwan6 <58982538+liwanwan6@users.noreply.github.com>
2019-12-18 22:04:58 +0800

    Add files via upload

---
+++ 开灯问题.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	int light, person;
+	cin >> light >> person;
+	int* light_array;
+	light_array = new int[light+1]();
+	for (int i = 1; i <=person; i++)
+	{
+		for (int j = 1; j <= light; j++)
+		{
+			if (j % i == 0)
+			{
+				light_array[j] = !light_array[j];
+			}
+		}
+	}
+	for (int i = 1; i <= light; i++)
+	{
+		if (*(light_array+i))
+		{
+			cout <<left<<setw(3)<<  i;
+		}
+	}
+	return 0;
+}
