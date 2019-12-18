47e61f002f491f82f9d0d4d83621581e444817d0
liwanwan6 <58982538+liwanwan6@users.noreply.github.com>
2019-12-18 13:40:32 +0800

    Add files via upload

---
+++ 筛选法求素数.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	int n[100000+1] = {};
+	int number = 100000;
+	for (int i = 2; i <= number; i++)
+	{
+		if (!n[i])
+		{
+			for (int j = i * 2; j <= number; j += i)
+			{
+				n[j] = 1;
+			}
+		}
+	}
+	for (int i = 2; i <= number + 1; i++)
+	{
+		if (!n[i])
+		{
+			cout <<left<< setw(8) << i;
+		}
+	}
+		return 0;
+}
