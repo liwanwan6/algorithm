7a1859b0fb0a36d65f68c63fc5398b79f7569bf0
liwanwan6 <58982538+liwanwan6@users.noreply.github.com>
2020-01-10 18:46:29 +0800

    Add files via upload

---
+++ 猜数字游戏的提示.c
@@ -0,0 +1,55 @@
+﻿#include<stdio.h>
+int main()
+{
+	int n, key[1024], answer[1024], count = 0;;
+	/*长度*/
+	while (scanf("%d", &n) == 1 && n)
+	{	
+		printf("Game%d:", ++count);
+		/*正确答案*/
+		for (int i = 0; i < n; i++)
+		{
+			scanf("%d", &key[i]);
+		}
+		for (;;)
+		{		
+			int A = 0, B = 0;
+			int sum = 0;
+			/*用户答案*/
+			for (int j = 0; j < n; j++)
+			{
+				scanf("%d", &answer[j]);
+				/*判断同位字符*/
+				if (key[j] == answer[j])
+				{
+					A++;
+				}
+				sum += answer[j];
+			}
+			/*判断是否退出*/
+			if (!sum)
+			{
+				break;
+			}
+			/*判断同字符，不同位*/
+			for (int j = 1; j < 10; j++)
+			{
+				int a = 0, b = 0;
+				for (int x = 0; x < n; x++)
+				{
+					if (key[x] == j)
+					{
+						a++;
+					}
+					if (answer[x] == j)
+					{
+						b++;
+					}
+				}
+				B += a > b ? b : a;
+			}
+			printf("(%d,%d)\n", A, B - A);
+		}
+	}
+	return 0;
+}
