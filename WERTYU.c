3d7540901f168a2cc144051f3a02198de655baf3
liwanwan6 <58982538+liwanwan6@users.noreply.github.com>
2020-01-10 13:13:43 +0800

    Add files via upload

---
+++ WERTYU.c
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char array[] = "1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
+	char c;
+	while((c=getchar())!=EOF)
+	{
+		char* find = strchr(array, c);
+		if (find)
+		{
+			putchar(*(find-1));
+		}
+		else
+		{
+			putchar(c);
+		}
+	}
+	return 0;
+}
