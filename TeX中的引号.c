c7e2a9fffb7ee01086f70675242d783bc198728c
liwanwan6 <58982538+liwanwan6@users.noreply.github.com>
2020-01-10 12:33:55 +0800

    Add files via upload

---
+++ TeX中的引号.c
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+int main()
+{
+	char c;
+	int choice = 1;
+	while ((c = getchar()) != EOF)
+	{
+		if (c == '"')
+		{
+			printf("%s", choice ? "“": "”");
+			choice = !choice;
+		}
+		else
+		{
+			printf("%c", c);
+		}
+	}
+	return 0;
+}
